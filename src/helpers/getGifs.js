cdc27b74cfb93debad104dd96eb64ca056b77fdb
diegocra2603 <diegocra2603@gmail.com>
2022-04-21 05:35:56 -0600

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+
+
+export const getGifts = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=YilBMg9TKxYmCU5almcSqZvYB3S38R8T`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifts = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifts;
+
+}
